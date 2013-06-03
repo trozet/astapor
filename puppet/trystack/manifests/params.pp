111d46ffb52ca2dbef0a77a592541066d6b86b0c
Greg Sutcliffe <gsutclif@redhat.com>
2013-06-03 14:24:54 +0100

    Use CIDR for network ranges, numerical netmask doesn't work

--- puppet/trystack/manifests/params.pp
+++ puppet/trystack/manifests/params.pp
@@ -16,11 +16,11 @@ class trystack::params {
   $nova_db_password           = 'CHANGEME'
   $nova_user_password         = 'CHANGEME'
 
-  # Networking
+  # Networking - we're assuming /24 ranges, but the user can always override
   $public_interface           = 'PRIMARY'
   $private_interface          = 'SECONDARY'
-  $fixed_network_range        = inline_template("<%= scope.lookupvar('::network_${private_interface}') + '/' + scope.lookupvar('::netmask_${private_interface}') %>")
-  $floating_network_range     = inline_template("<%= scope.lookupvar('::network_${public_interface}') + '/' + scope.lookupvar('::netmask_${public_interface}') %>")
+  $fixed_network_range        = inline_template("<%= scope.lookupvar('::network_${private_interface}') + '/24' %>")
+  $floating_network_range     = inline_template("<%= scope.lookupvar('::network_${public_interface}') + '/24' %>")
   $pacemaker_priv_floating_ip = 'PRIV_IP'
   $pacemaker_pub_floating_ip  = 'PUB_IP'
 
