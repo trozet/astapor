cf844799321a8b0cb0107628402fc6f37c30e149
Jiri Stransky <jistr@redhat.com>
2013-09-24 13:05:11 +0200

    OpenStack Block Storage host group

--- puppet/modules/quickstack/manifests/cinder_storage.pp
+++ puppet/modules/quickstack/manifests/cinder_storage.pp
@@ -1,9 +1,9 @@
 class quickstack::cinder_storage(
-  $cinder_db_password,
-  $controller_priv_floating_ip,
-  $private_interface,
-  $verbose,
-) {
+  $cinder_db_password          = $quickstack::params::cinder_db_password,
+  $controller_priv_floating_ip = $quickstack::params::controller_priv_floating_ip,
+  $private_interface           = $quickstack::params::private_interface,
+  $verbose                     = $quickstack::params::verbose,
+) inherits quickstack::params {
 
   class { 'cinder':
     rpc_backend    => 'cinder.openstack.common.rpc.impl_qpid',
