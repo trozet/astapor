f6396fef4348b10c7427d248f3735e2ea1bb092f
Jiri Stransky <jistr@redhat.com>
2013-10-01 12:45:51 +0200

    heat_controller puppet class to reduce code duplication
    Move common Heat code from Neutron-based and Nova-Network-based
    controller classes to a common class which is then included.

--- puppet/modules/quickstack/manifests/neutron/controller.pp
+++ puppet/modules/quickstack/manifests/neutron/controller.pp
@@ -78,26 +78,6 @@ class quickstack::neutron::controller (
         internal_address => $controller_priv_floating_ip,
     }
 
-    if $heat_cfn == true {
-      class {"heat::keystone::auth":
-        password => $heat_user_password,
-        heat_public_address => $controller_priv_floating_ip,
-        heat_admin_address => $controller_priv_floating_ip,
-        heat_internal_address => $controller_priv_floating_ip,
-        cfn_public_address => $controller_priv_floating_ip,
-        cfn_admin_address => $controller_priv_floating_ip,
-        cfn_internal_address => $controller_priv_floating_ip,
-      }      
-    } else {
-       class {"heat::keystone::auth":
-         password => $heat_user_password,
-         heat_public_address => $controller_priv_floating_ip,
-         heat_admin_address => $controller_priv_floating_ip,
-         heat_internal_address => $controller_priv_floating_ip,
-         cfn_auth_name => undef,
-       }
-    }
-
     class {'openstack::glance':
         db_host        => $controller_priv_floating_ip,
         user_password  => $glance_user_password,
@@ -175,44 +155,6 @@ class quickstack::neutron::controller (
         require           => Class['mongodb'],
     }
 
-    class { 'heat':
-        keystone_host     => $controller_priv_floating_ip,
-        keystone_password => $heat_user_password,
-        auth_uri          => "http://${controller_priv_floating_ip}:35357/v2.0",
-        rpc_backend       => 'heat.openstack.common.rpc.impl_qpid',
-        qpid_hostname     => $controller_priv_floating_ip,
-        verbose           => $verbose,
-    }
-
-    if $heat_cfn == true {
-      class { 'heat::api_cfn':
-      }
-    }
-
-    if $heat_cloudwatch == true {
-      
-      class { 'heat::api_cfn':
-      }
-    }
-    
-    class {'heat::db::mysql':
-        password => $heat_db_password,
-        allowed_hosts => "%%",
-    }
-
-    class {'heat::db':
-       sql_connection => "mysql://heat:${heat_db_password}@${controller_floating_ip}/heat"
-    }
-
-    class {'heat::api':
-    }
-
-    class {'heat::engine':
-        heat_metadata_server_url      => "http://${controller_priv_floating_ip}:8000",
-        heat_waitcondition_server_url => "http://${controller_priv_floating_ip}:8000/v1/waitcondition",
-        heat_watch_server_url         => "http://${controller_priv_floating_ip}:8003",
-    }
-
     glance_api_config {
         'DEFAULT/notifier_strategy': value => 'qpid'
     }
@@ -220,6 +162,15 @@ class quickstack::neutron::controller (
     class { 'quickstack::cinder_controller':
       cinder_db_password          => $cinder_db_password,
       cinder_user_password        => $cinder_user_password,
+      controller_priv_floating_ip => $controller_priv_floating_ip,
+      verbose                     => $verbose,
+    }
+
+    class { 'quickstack::heat_controller':
+      heat_cfn                    => $heat_cfn,
+      heat_cloudwatch             => $heat_cloudwatch,
+      heat_user_password          => $heat_user_password,
+      heat_db_password            => $heat_db_password,
       controller_priv_floating_ip => $controller_priv_floating_ip,
       verbose                     => $verbose,
     }
