9801822bb871c9cab00affeafec3c7df3f344c72
Gilles Dubreuil <gdubreui@redhat.com>
2013-10-31 23:07:26 +1100

    Initial glusterfs

--- puppet/modules/quickstack/manifests/cinder_storage.pp
+++ puppet/modules/quickstack/manifests/cinder_storage.pp
@@ -1,5 +1,10 @@
-class quickstack::cinder_storage(
+# Cinder Storage
+class quickstack::cinder_storage (
+  $cinder_backend_gluster      = $quickstack::params::cinder_backend_gluster,
+  $cinder_backend_iscsi        = $quickstack::params::cinder_backend_iscsi,
   $cinder_db_password          = $quickstack::params::cinder_db_password,
+  $cinder_gluster_volume       = $quickstack::params::cinder_gluster_volume,
+  $cinder_gluster_peers        = $quickstack::params::cinder_gluster_peers,
   $controller_priv_floating_ip = $quickstack::params::controller_priv_floating_ip,
   $private_interface           = $quickstack::params::private_interface,
   $mysql_host                  = $quickstack::params::mysql_host,
@@ -17,13 +22,24 @@ class quickstack::cinder_storage(
 
   class { 'cinder::volume': }
 
-  class { 'cinder::volume::iscsi':
-    iscsi_ip_address => getvar("ipaddress_${private_interface}"),
+  if $cinder_backend_gluster == true {
+    class { 'gluster::client': }
+
+    class { 'cinder::volume::glusterfs':
+      glusterfs_mount_point_base => '/var/lib/cinder/volumes',
+      glusterfs_shares           => suffix($cinder_gluster_peers, ":/${cinder_gluster_volume}")
+    }
   }
 
-  firewall { '010 cinder iscsi':
-      proto => 'tcp',
-      dport => ['3260'],
+  if $cinder_backend_iscsi == true {
+    class { 'cinder::volume::iscsi':
+      iscsi_ip_address => getvar("ipaddress_${private_interface}"),
+    }
+
+    firewall { '010 cinder iscsi':
+      proto  => 'tcp',
+      dport  => ['3260'],
       action => 'accept',
+    }
   }
 }
