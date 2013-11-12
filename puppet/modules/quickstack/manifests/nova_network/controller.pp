ef0fbdd5727d4818fedca3320a7239f7be064469
Gilles Dubreuil <gdubreui@redhat.com>
2013-11-12 18:07:05 +1100

    Fixed class name comment

--- puppet/modules/quickstack/manifests/nova_network/controller.pp
+++ puppet/modules/quickstack/manifests/nova_network/controller.pp
@@ -1,6 +1,5 @@
 # Quickstart class for nova network controller
-class quickstack::nova_network::controller ()
-inherits quickstack::params {
+class quickstack::nova_network::controller () inherits quickstack::params {
    nova_config {
     'DEFAULT/auto_assign_floating_ip': value => 'True';
     'DEFAULT/multi_host':              value => 'True';
