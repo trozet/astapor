76930baaa67c20a7f007bdfd2e692846a5252cc1
Jiri Stransky <jistr@redhat.com>
2013-11-13 13:40:22 +0100

    Split cinder_storage class
    The cinder_storage class served two different purposes:
    
    * mounting of Gluster volumes into controller,
    
    * providing a LVM block storage backend accessible through iSCSI.
    
    Mounting of Gluster volumes has moved into cinder_controller, and the
    LVM block storage backend has moved into
    storage_backend::lvm_cinder. The cinder_storage class has been
    removed.

---
+++ puppet/modules/quickstack/manifests/storage_backend/lvm_cinder.pp
@@ -0,0 +1,29 @@
+class quickstack::storage_backend::lvm_cinder(
+  $cinder_db_password          = $quickstack::params::cinder_db_password,
+  $controller_priv_floating_ip = $quickstack::params::controller_priv_floating_ip,
+  $private_interface           = $quickstack::params::private_interface,
+  $mysql_host                  = $quickstack::params::mysql_host,
+  $qpid_host                   = $quickstack::params::qpid_host,
+  $verbose                     = $quickstack::params::verbose,
+) inherits quickstack::params {
+
+  class { 'cinder':
+    rpc_backend    => 'cinder.openstack.common.rpc.impl_qpid',
+    qpid_hostname  => $qpid_host,
+    qpid_password  => 'guest',
+    sql_connection => "mysql://cinder:${cinder_db_password}@${mysql_host}/cinder",
+    verbose        => $verbose,
+  }
+
+  class { 'cinder::volume': }
+
+  class { 'cinder::volume::iscsi':
+    iscsi_ip_address => getvar("ipaddress_${private_interface}"),
+  }
+
+  firewall { '010 cinder iscsi':
+      proto => 'tcp',
+      dport => ['3260'],
+      action => 'accept',
+  }
+}
