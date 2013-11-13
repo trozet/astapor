e02e639d55db2c622581671c79fac11caefdc6f4
Jiri Stransky <jistr@redhat.com>
2013-11-13 17:15:37 +0100

    Cascade parameters from storage_backend::gluster
    We need to pass parameters needed for storage_backend::gluster::cinder
    and storage_backend::gluster::glance into storage_backend::gluster (top
    class for Gluster Storage host group), because we want Foreman to
    override them, instead of inheriting values from quickstack::params.

--- puppet/modules/quickstack/manifests/storage_backend/gluster.pp
+++ puppet/modules/quickstack/manifests/storage_backend/gluster.pp
@@ -1,18 +1,34 @@
 # quickstack storage class
-class quickstack::storage_backend::gluster {
+class quickstack::storage_backend::gluster(
+  $cinder_gluster_path          = $quickstack::params::cinder_gluster_path,
+  $cinder_gluster_peers         = $quickstack::params::cinder_gluster_peers,
+  $cinder_gluster_replica_count = $quickstack::params::cinder_gluster_replica_count,
+  $glance_gluster_path          = $quickstack::params::glance_gluster_path,
+  $glance_gluster_peers         = $quickstack::params::glance_gluster_peers,
+  $glance_gluster_replica_count = $quickstack::params::glance_gluster_replica_count,
+) inherits quickstack::params {
+
   class { 'gluster::server': }
 
-  class { 'quickstack::storage_backend::gluster::volume_cinder': }
- 
-  class { 'quickstack::storage_backend::gluster::volume_glance': } 
+  class { 'quickstack::storage_backend::gluster::volume_cinder':
+    cinder_gluster_path          => $cinder_gluster_path,
+    cinder_gluster_peers         => $cinder_gluster_peers,
+    cinder_gluster_replica_count => $cinder_gluster_replica_count,
+  }
 
-#  class { 'quickstack::storage_backend::gluster::volume_swift': } 
+  class { 'quickstack::storage_backend::gluster::volume_glance':
+    glance_gluster_path          => $glance_gluster_path,
+    glance_gluster_peers         => $glance_gluster_peers,
+    glance_gluster_replica_count => $glance_gluster_replica_count,
+  }
+
+#  class { 'quickstack::storage_backend::gluster::volume_swift': }
 
   firewall { '001 RPC and gluster daemon incoming':
     proto  => 'tcp',
     dport  => [ '111', '24007', '24008' ],
     action => 'accept',
-  } 
+  }
 
   # 1 port per brick
   firewall { '002 gluster bricks incoming':
@@ -20,5 +36,5 @@ class quickstack::storage_backend::gluster {
     #dport  => port_range('24009', '6'),
     dport    => [ '24009', '24010', '24011', '24012', '24013', '24014' ],
     action => 'accept',
-  } 
+  }
 }
