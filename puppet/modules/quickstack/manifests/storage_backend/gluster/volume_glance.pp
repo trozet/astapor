8be2765679976c61121b0a6ebf8631eb350baa5a
Jiri Stransky <jistr@redhat.com>
2013-11-19 14:53:39 +0100

    Honor parameter values for naming gluster volumes
    $cinder_gluster_volume and $glance_gluster_volume parameters used to
    affect only controller, but we should also use them in Gluster
    backend. We need them synced if we want mounting on controller to
    work.

--- puppet/modules/quickstack/manifests/storage_backend/gluster/volume_glance.pp
+++ puppet/modules/quickstack/manifests/storage_backend/gluster/volume_glance.pp
@@ -3,11 +3,12 @@ class quickstack::storage_backend::gluster::volume_glance (
   $glance_gluster_path          = $quickstack::params::glance_gluster_path,
   $glance_gluster_peers         = $quickstack::params::glance_gluster_peers,
   $glance_gluster_replica_count = $quickstack::params::glance_gluster_replica_count,
+  $glance_gluster_volume        = $quickstack::params::glance_gluster_volume,
 ) inherits quickstack::params {
-  volume { 'glance':
+  volume { $glance_gluster_volume:
     ensure         => present,
     path           => $glance_gluster_path,
     peers          => $glance_gluster_peers,
-    replica_count  => $glance_gluster_replica_count, 
+    replica_count  => $glance_gluster_replica_count,
   }
 }
