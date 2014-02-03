104044327c0d147270e625b30ab49483019791d0
Jason Guiditta <jguiditt@redhat.com>
2014-02-03 10:42:49 -0500

    Refactor Load Balancer.
    * Move the define into it's own class
    * Clean up other puppet lint warnings.

--- puppet/modules/quickstack/manifests/load_balancer.pp
+++ puppet/modules/quickstack/manifests/load_balancer.pp
@@ -185,31 +185,3 @@ class quickstack::load_balancer (
 
   sysctl::value { 'net.ipv4.ip_nonlocal_bind': value => '1' }
 }
-
-define quickstack::load_balancer::proxy (
-  $addr,
-  $port,
-  $mode,
-  $listen_options,
-  $member_options,
-  $define_cookies = false,
-) {
-  include quickstack::load_balancer
-
-  haproxy::listen { $name:
-    ipaddress => $addr,
-    ports     => $port,
-    mode      => $mode,
-    options   => $listen_options,
-    collect_exported => false,
-  }
-
-  haproxy::balancermember { $name:
-    listening_service => $name,
-    ports             => $port,
-    server_names      => $quickstack::load_balancer::backend_server_names,
-    ipaddresses       => $quickstack::load_balancer::backend_server_addrs,
-    options           => $member_options,
-    define_cookies    => $define_cookies,
-  }
-}
