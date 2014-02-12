3b5bcdf9a33600b7995b6811094996c22d654d94
Rob Parrott <robparrott@gmail.com>
2014-02-12 12:33:15 -0500

    Add in the option to create a keystonerc_admin file in root's home directory.
    (cherry picked from commit 07b0e793403a65a3e0e6b0ca56dd7e874b4ccbc9)

--- puppet/modules/quickstack/manifests/controller_common.pp
+++ puppet/modules/quickstack/manifests/controller_common.pp
@@ -21,6 +21,7 @@ class quickstack::controller_common (
   $horizon_secret_key            = $quickstack::params::horizon_secret_key,
   $keystone_admin_token          = $quickstack::params::keystone_admin_token,
   $keystone_db_password          = $quickstack::params::keystone_db_password,
+  $keystonerc                    = false,
   $neutron_metadata_proxy_secret = $quickstack::params::neutron_metadata_proxy_secret,
   $mysql_host                    = $quickstack::params::mysql_host,
   $mysql_root_password           = $quickstack::params::mysql_root_password,
@@ -333,4 +334,12 @@ class quickstack::controller_common (
       persistent => true,
     }
   }
+
+  if str2bool_i("$keystonerc") {
+    class { 'quickstack::admin_client':
+      admin_password        => $admin_password,
+      controller_admin_host => $controller_admin_host,
+    }
+  }
+
 }
