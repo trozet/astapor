95eee05b9154f7dff2c1cfda39501e7615ec57bf
Crag Wolfe <cwolfe@redhat.com>
2014-03-19 06:53:59 -0400

    BZ #1049121 -  Install a Highly Available ... Foreman (HA/mysql)
    https://bugzilla.redhat.com/show_bug.cgi?id=1049121
    
    The custom fact, $hamysql_active_node, did not behave as expected
    during an initial puppet run if pcs was not yet installed: it would
    return true rather than the expected false.  Also, if there were
    "Failed actions" in the output of "pcs status", the fact would always
    incorrectly return false.  Both of these issues are fixed.
    
    Additionally, the primary HA/mysql puppet manifest, node.pp, as been
    updated to work with the lastest puppet-pacemaker module.
    
    (cherry picked from commit 3e6460f953ba088734cefbd47d2430fcf26af3bc)

--- puppet/modules/quickstack/lib/facter/hamysql_active_node.rb
+++ puppet/modules/quickstack/lib/facter/hamysql_active_node.rb
@@ -1,8 +1,12 @@
 Facter.add("hamysql_active_node") do
 
   setcode do
-    a = Facter::Util::Resolution.exec("/usr/sbin/pcs status | grep mysql-ostk-mysql | perl -p -e 's/^.*Started (\\S*).*$/$1/'")
-    b = Facter::Util::Resolution.exec("/usr/sbin/crm_node -n")
-    a==b
+    if File.exist? "/usr/sbin/pcs" and File.exist? "/usr/sbin/crm_node"
+      a = Facter::Util::Resolution.exec("/usr/sbin/pcs status | grep -P 'mysql-ostk-mysql\\s.*Started' | perl -p -e 's/^.*Started (\\S*).*$/$1/' 2>&1")
+      b = Facter::Util::Resolution.exec("/usr/sbin/crm_node -n 2>&1")
+      a==b
+    else
+     false
+    end
   end
 end
