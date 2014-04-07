6a89136bc4e210688ec2f6f6754182592bcc9b61
Jason Guiditta <jguiditt@redhat.com>
2014-04-07 11:39:11 -0400

    BZ #1075818 - Fixes for HA load  balancer.
    https://bugzilla.redhat.com/show_bug.cgi?id=1075818
    
    - Use new VIP define
    - Move params to params.pp
    - add backend block to quickstack::load_balancer

--- puppet/modules/quickstack/manifests/load_balancer.pp
+++ puppet/modules/quickstack/manifests/load_balancer.pp
@@ -163,6 +163,8 @@ class quickstack::load_balancer (
     mode => 'http',
     listen_options => { 'option' => [ 'httplog' ] },
     member_options => [ 'check' ],
+    backend_server_addrs => $backend_server_addrs,
+    backend_server_names => $backend_server_names,
   }
   quickstack::load_balancer::proxy { 'ceilometer-api':
     addr => [ $controller_pub_host,
