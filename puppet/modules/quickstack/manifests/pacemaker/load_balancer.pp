f45fcfc04ecaaef0430c7919c6d1ad86daed3b77
Crag Wolfe <cwolfe@redhat.com>
2014-04-17 22:15:42 -0400

    BZ #1075818 - HA controller more strict ordering
    https://bugzilla.redhat.com/show_bug.cgi?id=1075818
    
    Never create a cloned resource unless it is available on all nodes.
    
    (cherry picked from commit 64017c4f155ca0e770b589b9305d814d0337c6e7)

--- puppet/modules/quickstack/manifests/pacemaker/load_balancer.pp
+++ puppet/modules/quickstack/manifests/pacemaker/load_balancer.pp
@@ -8,8 +8,19 @@ class quickstack::pacemaker::load_balancer {
     public_vip  => map_params("loadbalancer_public_vip"),
     private_vip => map_params("loadbalancer_private_vip"),
     admin_vip   => map_params("loadbalancer_admin_vip"),
-  }
-  ->
+  } ->
+
+  Service['haproxy'] ->
+  exec {"pcs-haproxy-server-set-up-on-this-node":
+    command => "/tmp/ha-all-in-one-util.bash update_my_node_property haproxy"
+  } ->
+  exec {"all-haproxy-nodes-are-up":
+    timeout   => 3600,
+    tries     => 360,
+    try_sleep => 10,
+    command   => "/tmp/ha-all-in-one-util.bash all_members_include haproxy",
+
+  } ->
   pacemaker::resource::lsb {'haproxy':
     group => "$loadbalancer_group",
     clone => true,
