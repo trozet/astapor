7a89ef546c9d5efdd972cd776c1aca9df815fac1
Crag Wolfe <cwolfe@redhat.com>
2014-04-18 00:28:50 -0400

    More Fixes for Heat.
      - Add auth_encryption_key which is now required.
      - sql_connection string moved to ::heat since ::heat::db no longer
        exists.
    
    (cherry picked from commit 592630365d11f6a80e6a692c63d41fd5ceba88cc)

--- puppet/modules/quickstack/manifests/heat_controller.pp
+++ puppet/modules/quickstack/manifests/heat_controller.pp
@@ -1,12 +1,13 @@
 class quickstack::heat_controller(
+  $auth_encryption_key,
   $heat_cfn,
   $heat_cloudwatch,
   $heat_user_password,
   $heat_db_password,
   $controller_priv_host,
   $controller_pub_host,
-  $mysql_host,
   $mysql_ca,
+  $mysql_host,
   $ssl,
   $qpid_host,
   $qpid_port,
@@ -16,6 +17,11 @@ class quickstack::heat_controller(
   $verbose,
 ) {
 
+  if str2bool_i("$ssl") {
+    $sql_connection = "mysql://heat:${heat_db_password}@${mysql_host}/heat?ssl_ca=${mysql_ca}"
+  } else {
+    $sql_connection = "mysql://heat:${heat_db_password}@${mysql_host}/heat"
+  }
   class {"::heat::keystone::auth":
       password         => $heat_user_password,
       public_address   => $controller_pub_host,
@@ -41,6 +47,7 @@ class quickstack::heat_controller(
       qpid_username     => $qpid_username,
       qpid_password     => $qpid_password,
       verbose           => $verbose,
+      sql_connection    => $sql_connection,
   }
 
   class { '::heat::api_cfn':
@@ -52,24 +59,17 @@ class quickstack::heat_controller(
   }
 
   class { '::heat::engine':
+      auth_encryption_key           => $auth_encryption_key,
       heat_metadata_server_url      => "http://${controller_priv_host}:8000",
       heat_waitcondition_server_url => "http://${controller_priv_host}:8000/v1/waitcondition",
       heat_watch_server_url         => "http://${controller_priv_host}:8003",
   }
 
-  class { '::heat::db::mysql':
-      password => $heat_db_password,
-      allowed_hosts => "%%",
-  }
-
-  if str2bool_i("$ssl") {
-    $sql_connection = "mysql://heat:${heat_db_password}@${mysql_host}/heat?ssl_ca=${mysql_ca}"
-  } else {
-    $sql_connection = "mysql://heat:${heat_db_password}@${mysql_host}/heat"
-  }
-
-  class { 'heat::db':
-      sql_connection => $sql_connection,
+  # TODO: this ain't no place to be creating a db locally as happens below
+  class { 'heat::db::mysql':
+    password      => $heat_db_password,
+    host          => $mysql_host,
+    allowed_hosts => "%%",
   }
 
   class { '::heat::api':
