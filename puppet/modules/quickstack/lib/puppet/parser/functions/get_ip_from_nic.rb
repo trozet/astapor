603d0e31082685b35884a44619f35ccdc40b33be
Crag Wolfe <cwolfe@redhat.com>
2014-04-23 00:10:46 -0400

    get_nic_from_network function fixes
    Do not use a Facter:: call within a function since it seems
    Foreman-host specific rather than client-node specific (and we just
    care about client facts/variables).
    
    Also, make sure not to call gsub on a nil object to avoid mysterious
    stack traces.

--- puppet/modules/quickstack/lib/puppet/parser/functions/get_ip_from_nic.rb
+++ puppet/modules/quickstack/lib/puppet/parser/functions/get_ip_from_nic.rb
@@ -8,6 +8,9 @@ EOS
     raise(Puppet::ParseError, "get_ip_from_nic(): Wrong number of arguments " +
       "given (#{arguments.size} for 1)") if arguments.size < 1
 
+    if (arguments[0] == nil)
+      return nil
+    end
     the_nic = arguments[0].gsub(/[.:-]+/,'_')
     ip = lookupvar("ipaddress_#{the_nic}")
 
