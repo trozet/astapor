d38fe378639b04787a282947ea8b414c944b23d8
Jiri Stransky <jistr@redhat.com>
2014-04-24 11:06:33 +0200

    Open port for rsync

--- puppet/modules/quickstack/manifests/rsync/common.pp
+++ puppet/modules/quickstack/manifests/rsync/common.pp
@@ -5,4 +5,10 @@ class quickstack::rsync::common ( ) {
   package { 'xinetd':
     ensure => installed,
   }
+
+  firewall { '010 rsync incoming':
+    proto  => 'tcp',
+    dport  => ["873"],
+    action => 'accept',
+  }
 }
