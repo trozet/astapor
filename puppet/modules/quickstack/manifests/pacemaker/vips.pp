7881bf5d3740ad5ef2215a24385b7f5b8a336130
Jason Guiditta <jguiditt@redhat.com>
2014-04-25 13:41:48 -0400

    Remove grouping on vips.
    (cherry picked from commit a2c92c55fc39d2cd38a1ed3056e4a42818922c70)

--- puppet/modules/quickstack/manifests/pacemaker/vips.pp
+++ puppet/modules/quickstack/manifests/pacemaker/vips.pp
@@ -7,20 +7,17 @@ define quickstack::pacemaker::vips(
 
   pacemaker::resource::ip { "ip-${pcmk_group}_${public_vip}":
     ip_address => "$public_vip",
-    group => "$pcmk_group",
   }
 
-  if ( $public_vip != $private_vip ) { 
+  if ( $public_vip != $private_vip ) {
     pacemaker::resource::ip { "ip-${pcmk_group}_${private_vip}":
       ip_address => "$private_vip",
-      group      => "$pcmk_group",
     }
   }
 
-  if ( ($admin_vip != $private_vip) and ($admin_vip != $public_vip) ) { 
+  if ( ($admin_vip != $private_vip) and ($admin_vip != $public_vip) ) {
     pacemaker::resource::ip { "ip-${pcmk_group}_${admin_vip}":
       ip_address => "$admin_vip",
-      group      => "$pcmk_group",
     }
   }
 }
