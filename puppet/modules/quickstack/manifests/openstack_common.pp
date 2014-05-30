def078b593a41dad72bd2721d360cdb069414849
Crag Wolfe <cwolfe@redhat.com>
2014-05-30 00:14:15 -0400

    BZ #1103271 - puppet modules need to handle missing openstack-selinux gracefully
    https://bugzilla.redhat.com/show_bug.cgi?id=1103271
    
    Do not install openstack-selinux on el7, just selinux-policy.

--- puppet/modules/quickstack/manifests/openstack_common.pp
+++ puppet/modules/quickstack/manifests/openstack_common.pp
@@ -2,9 +2,15 @@
 class quickstack::openstack_common(
 ) inherits quickstack::params {
 
-  if str2bool($::selinux) and $::operatingsystem != "Fedora" {
-    package{ 'openstack-selinux':
-        ensure => present,
+  # openstack-selinux does not exist in el7 yet, but it when it does
+  # can just remove the ::operatingsystemrelease clause below
+  if (str2bool($::selinux) and $::operatingsystem != "Fedora") {
+    if ($::operatingsystemrelease =~ /^6\..*$/)  {
+      package{ 'openstack-selinux':
+          ensure => present, }
+    } else {
+      package{ 'selinux-policy':
+          ensure => present, }
     }
   }
 
