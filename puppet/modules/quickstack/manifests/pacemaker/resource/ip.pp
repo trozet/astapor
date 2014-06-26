9c0cbd866cdeed1af7b2fbe4bf8efaf037b52244
John Eckersberg <jeckersb@redhat.com>
2014-06-26 11:19:06 -0400

    Add monitor_params for pacemaker resources

--- puppet/modules/quickstack/manifests/pacemaker/resource/ip.pp
+++ puppet/modules/quickstack/manifests/pacemaker/resource/ip.pp
@@ -3,16 +3,18 @@ define quickstack::pacemaker::resource::ip($ip_address,
                                $nic='',
                                $group='',
                                $interval='30s',
+                               $monitor_params=undef,
                                $ensure='present') {
   include quickstack::pacemaker::params
 
   if has_interface_with("ipaddress", map_params("cluster_control_ip")){
     ::pacemaker::resource::ip{ "$name": 
-                            ip_address   => $ip_address,
-                            cidr_netmask => $cidr_netmask,
-                            nic          => $nic,
-                            group        => $group,
-                            interval     => $interval,
-                            ensure       => $ensure }
+                            ip_address     => $ip_address,
+                            cidr_netmask   => $cidr_netmask,
+                            nic            => $nic,
+                            group          => $group,
+                            interval       => $interval,
+                            monitor_params => $monitor_params,
+                            ensure         => $ensure }
   }
 }
