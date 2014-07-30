42df52327e907613db8530facdcb94366cb11856
Jiri Stransky <jistr@redhat.com>
2014-07-30 17:27:04 +0200

    BZ#1122701 - Incoming ports are wide open
    https://bugzilla.redhat.com/show_bug.cgi?id=1122701
    
    Uninstall firewalld. The firewall puppet module will then take care of
    installing iptables-services package and enabling and starting of
    iptables. Default firewall rules will then be present.
    
    Moving the disabling of firewalld to firewall::common so that it's
    also ran for the Gluster server host group.

--- puppet/modules/quickstack/manifests/firewall/common.pp
+++ puppet/modules/quickstack/manifests/firewall/common.pp
@@ -1,4 +1,14 @@
 class quickstack::firewall::common {
 
+  # for any Fedora, and for Enterprise Linux > 6
+  if ($::operatingsystem == 'Fedora' or
+      ($::operatingsystem != 'Fedora' and $::operatingsystemmajrelease > 6)) {
+    # Uninstall firewalld since everything uses iptables for now
+    package { 'firewalld':
+      ensure => "absent",
+      before => Package['iptables-services'],
+    }
+  }
+
   class { 'firewall': }
 }
