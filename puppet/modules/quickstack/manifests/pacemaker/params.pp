db1d4f8dfba297e957cce889c077d34f86a9ee5f
Tim Rozet <tdrozet@gmail.com>
2015-03-31 17:54:07 -0400

    Removes includes and ordering in params
    This really shouldn't be here as pure puppet, but it provides the basis
    of how quickstack works in Foreman.  With the OPNFV module we control
    the ordering and don't rely on Foreman smart variables/puppet structure.

--- puppet/modules/quickstack/manifests/pacemaker/params.pp
+++ puppet/modules/quickstack/manifests/pacemaker/params.pp
@@ -109,28 +109,28 @@ class quickstack::pacemaker::params (
                             "$pcmk_iface",
                             "$pcmk_ip")
 
-  include quickstack::pacemaker::common
-  include quickstack::pacemaker::load_balancer
-  include quickstack::pacemaker::galera
-  include quickstack::pacemaker::qpid
-  include quickstack::pacemaker::keystone
-  include quickstack::pacemaker::swift
-  include quickstack::pacemaker::glance
-  include quickstack::pacemaker::nova
-  include quickstack::pacemaker::cinder
-  include quickstack::pacemaker::heat
-  include quickstack::pacemaker::constraints
+#  include quickstack::pacemaker::common
+#  include quickstack::pacemaker::load_balancer
+#  include quickstack::pacemaker::galera
+#  include quickstack::pacemaker::qpid
+#  include quickstack::pacemaker::keystone
+#  include quickstack::pacemaker::swift
+#  include quickstack::pacemaker::glance
+#  include quickstack::pacemaker::nova
+#  include quickstack::pacemaker::cinder
+#  include quickstack::pacemaker::heat
+#  include quickstack::pacemaker::constraints
 
-  Class['::quickstack::pacemaker::common'] ->
-  Class['::quickstack::pacemaker::load_balancer'] ->
-  Class['::quickstack::pacemaker::galera'] ->
-  Class['::quickstack::pacemaker::qpid'] ->
-  Class['::quickstack::pacemaker::keystone'] ->
-  Class['::quickstack::pacemaker::swift'] ->
-  Class['::quickstack::pacemaker::glance'] ->
-  Class['::quickstack::pacemaker::nova'] ->
-  Class['::quickstack::pacemaker::cinder'] ->
-  Class['::quickstack::pacemaker::heat'] ->
-  Class['::quickstack::pacemaker::constraints']
+#  Class['::quickstack::pacemaker::common'] ->
+#  Class['::quickstack::pacemaker::load_balancer'] ->
+#  Class['::quickstack::pacemaker::galera'] ->
+#  Class['::quickstack::pacemaker::qpid'] ->
+#  Class['::quickstack::pacemaker::keystone'] ->
+#  Class['::quickstack::pacemaker::swift'] ->
+#  Class['::quickstack::pacemaker::glance'] ->
+#  Class['::quickstack::pacemaker::nova'] ->
+#  Class['::quickstack::pacemaker::cinder'] ->
+#  Class['::quickstack::pacemaker::heat'] ->
+#  Class['::quickstack::pacemaker::constraints']
 
 }
